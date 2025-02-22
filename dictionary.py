51e12e1bb351d88940103967f511f7e5860c29db
Gambuu0527 <gambuuu0527@gmail.com>
2025-02-22 14:11:37 +0800

    dictionary_first

---
+++ dictionary.py
@@ -0,0 +1,33 @@
+ # dictionary
+#1 Dictionary
+Boldoo_dic = {'name': 'Boldoo', 'gender': 'male', 'salary': '3000000', 'birthday': '1998/06/01'}
+Boldoo_dic['name']
+
+#2 Dictionary
+Nar_dic = {'name': 'Nar', 'gender': 'famale', 'salary': '250000', 'birthday': '1989/12/01'}
+Nar_dic['gender']
+
+
+#Costumer_dict
+costumer_dict = {'1': Boldoo_dic, '2': Nar_dic}
+
+#3 Dictionary
+Sar_dic= {'name': 'Sar', 'gender': 'famale', 'salary': '2715269', 'birthday': '2000/11/26'}
+Sar_dic['birthday']
+
+# add costumer_dict
+costumer_dict['3']= Sar_dic
+
+#4 Dictionary
+Zolboo_dic= {'name': 'Zolboo', 'gender': 'male', 'salary': '5000000', 'birthday': '1972/09/01'}
+Zolboo_dic['salary']
+
+
+#5 Dictionary, add birthday
+Monh_dic= {'name': 'Monh', 'gender': 'male', 'salary': '7000000'}
+Monh_dic['birthday'] = '1986/02/29'
+
+# pandas dataframe
+import pandas as pd
+
+
